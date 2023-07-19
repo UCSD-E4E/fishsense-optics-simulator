051bcf3130740bfaf2693047dd237c2a5f45d2be
anaiperez_ <ana_i_perez@brown.edu>
2023-07-19 14:34:23 -0700

    Updating example graphs

--- graph_examples.m
+++ graph_examples.m
@@ -1,18 +1,10 @@
-%Example graphs utilizing GraphRays function
+%Example graphs utilizing the GraphRays2 function
 
-%FORMAT OF INPUT PARAMETERS
-%dist=[d0,d1,d2]
-%indices=[ng,nw]
-%lim (any number for axis limits)
-%alpha_param=[a_start,a_step,a_end]
-%phi_param=[p_start,p_step,p_end]
+% GraphRays2(normal,dist,indices,lim,alpha_param,phi_param,label)
 
-%Alpha: 20 deg , Phi: 180 deg
-GraphRays([2,12.7,200],[1.492,1.345],40,[0*pi/180,10*pi/180,pi/3],[pi/2,pi,3*pi/2],'y')
-GraphRays([2,12.7,200],[1.492,1.345],40,[0*pi/180,20*pi/180,pi/3],[pi/2,pi,3*pi/2],'n')
+%Alpha: 20 deg , Phi: 180 deg (slice along the Z axis, X=0)
+GraphRays2([0;0;1],[2,12.7,200],[1.492,1.345],40,[0*pi/180,20*pi/180,pi/3],[pi/2,pi,3*pi/2],'n')
+GraphRays2([0;0;1],[2,12.7,200],[1.492,1.345],40,[0*pi/180,20*pi/180,pi/3],[pi/2,pi,3*pi/2],'y')
 
-%Alpha: 20 deg , Phi: 2 deg
-%GraphRays([2,12.7,200],[1.492,1.345],40,[0*pi/180,2*pi/180,pi/3],[pi/2,pi,3*pi/2],'n')
-
-%Alpha: 2 deg, Phi: 2 deg
-%GraphRays([2,12.7,200],[1.492,1.345],40,[0*pi/180,10*pi/180,pi/3],[0,10*pi/180,2*pi],'n')
+%Alpha: 20 deg , Phi: 20 deg
+%GraphRays2([0;0;1],[2,12.7,200],[1.492,1.345],40,[0*pi/180,20*pi/180,pi/3],[0,pi*20/180,2*pi],'n')
